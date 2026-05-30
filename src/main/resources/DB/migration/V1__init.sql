f03e8a8a0c2f1f659b034784d4be71d221f72c74
ShahriyarTT <shahriyaraliyev@gmail.com>
2026-05-30 12:16:39 +0400

    Homework #8 add list of books into init.sql

--- src/main/resources/DB/migration/V1__init.sql
+++ src/main/resources/DB/migration/V1__init.sql
@@ -16,6 +16,24 @@ CREATE TABLE orders (
                         closed_at TIMESTAMP
 );
 
+-- Sample books
+INSERT INTO books (title, author, price, available)
+VALUES
+    ('Sapiens', 'Yuval Noah Harari', 10.00, TRUE),
+    ('The 48 Laws of Power', 'Robert Greene', 24.99, TRUE),
+    ('The Art of War', 'Sun Tzu', 19.99, TRUE),
+    ('1984', 'George Orwell', 42.50, TRUE),
+    ('Principles', 'Ray Dalio', 30.50, TRUE),
+    ('Getting Things Done', 'David Allen', 19.99, TRUE),
+    ('Rich Dad Poor Dad', 'Robert Kiyosaki', 19.99, TRUE),
+    ('How Google Works', 'Eric Schmidt', 19.99, TRUE),
+    ('Endurance', 'Alfred Lansing', 19.99, TRUE),
+    ('Telling Lies', 'Paul Ekman', 19.99, TRUE),
+    ('The Hidden Life of Trees', 'Peter Wohlleben', 19.99, TRUE),
+    ('Muhammad', 'Martin Lings', 19.99, TRUE),
+    ('The Prince', 'Niccolo Machiavelli', 19.99, TRUE),
+    ('Clean Architecture', 'Robert C. Martin', 30.00, TRUE);
+
 -- Many-to-many join table for order items
 CREATE TABLE order_items (
                              order_id INT REFERENCES orders(id) ON DELETE CASCADE,
